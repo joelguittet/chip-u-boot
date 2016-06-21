8fc1e60e900cd235bdbf87a7eb2cfe858ab11b04
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-06-21 09:59:07 +0200

    mtd: ubi: fix typo
    s/ubi_coso_add_full_leb/ubi_conso_add_full_leb
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/ubi/eba.c
+++ drivers/mtd/ubi/eba.c
@@ -728,7 +728,7 @@ int ubi_eba_write_leb(struct ubi_device *ubi, struct ubi_volume *vol, int lnum,
 		if (full) {
 			int ret;
 
-			ret = ubi_coso_add_full_leb(ubi, vol_id, lnum, 0);
+			ret = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
 			if (ret)
 				ubi_warn(ubi,
 					 "failed to add LEB %d:%d to the full LEB list",
@@ -794,7 +794,7 @@ retry:
 	up_read(&ubi->fm_eba_sem);
 
 	if (full) {
-		err = ubi_coso_add_full_leb(ubi, vol_id, lnum, 0);
+		err = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
 		if (err)
 			ubi_warn(ubi,
 				 "failed to add LEB %d:%d to the full LEB list",
@@ -928,7 +928,7 @@ retry:
 	vol->used_ebs = used_ebs; //XXX
 	up_read(&ubi->fm_eba_sem);
 
-	err = ubi_coso_add_full_leb(ubi, vol_id, lnum, 0);
+	err = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
 	if (err)
 		ubi_warn(ubi, "failed to add LEB %d:%d to the full LEB list",
 			 vol_id, lnum);
@@ -1062,7 +1062,7 @@ retry:
 	if (full) {
 		int ret;
 
-		ret = ubi_coso_add_full_leb(ubi, vol_id, lnum, 0);
+		ret = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
 		if (ret)
 			ubi_warn(ubi,
 				"failed to add LEB %d:%d to the full LEB list",
@@ -1715,9 +1715,10 @@ int ubi_eba_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 				}
 
 				if (aeb->full)
-					ubi_coso_add_full_leb(ubi, vol->vol_id,
-							      aeb->desc.lnum,
-							      aeb->peb_pos);
+					ubi_conso_add_full_leb(ubi,
+							       vol->vol_id,
+							       aeb->desc.lnum,
+							       aeb->peb_pos);
 			}
 		}
 	}
